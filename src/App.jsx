3d604e4875c90f009bd1480cc68ac46d88717f51
Wesley Nilton <wesleynlimac@gmail.com>
2022-11-23 08:35:16 -0300

    Create welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Welcome from './components/Welcome/Welcome';
 import './App.css';
 
 function App() {
   return(
     <div className="App">
       <h1>React Quiz</h1>
+      <Welcome />
     </div>
   )
 }
