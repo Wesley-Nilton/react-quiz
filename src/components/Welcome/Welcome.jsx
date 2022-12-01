888e65095c6f050c44ee034e77be8a1b7e10d1ea
Wesley Nilton <wesleynlimac@gmail.com>
2022-12-01 12:05:00 -0300

    Consumes context

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,7 +1,11 @@
+import {useContext} from 'react';
 import Quiz from '../../assets/img/quiz.svg';
+import {QuizContext} from '../../context/quiz';
 import './Welcome.css';
 
 function Welcome(){
+    const quizState = useContext(QuizContext);
+    console.log(quizState);
     return(
         <div id='welcome'>
             <h2>Seja bem-vindo</h2>
