7272d4826e67424d79bd98c30a7ac87847a44717
Wesley Nilton <wesleynlimac@gmail.com>
2022-12-06 16:16:34 -0300

    Add conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import {useContext} from 'react';
+import {QuizContext} from './context/quiz';
 import Welcome from './components/Welcome/Welcome';
+import Question from './components/Question/Question';
 import './App.css';
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
   return(
     <div className="App">
       <h1>React Quiz</h1>
-      <Welcome />
+      {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Playing' && <Question />}
     </div>
   )
 }
