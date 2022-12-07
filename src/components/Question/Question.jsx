b4d473e215533cfb2d3b9c349d48c89f67b6e632
Wesley Nilton <wesleynlimac@gmail.com>
2022-12-07 15:30:21 -0300

    Create question component structure

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import {useContext} from 'react';
+import {QuizContext} from '../../context/quiz';
 
-function Questions(){
+function Question(){
+    const [quizState, dispatch] = useContext(QuizContext);
     return(
-        <div>Question</div>
+        <div id='question'>
+            <p>Pergunta 0 de 10</p>
+            <h2>Pergunta atual</h2>
+            <div id="opstions-container">
+                <p>Opções</p>
+            </div>
+            <button>Continuar</button>
+        </div>
     )
 }
 
-export default Questions;
+export default Question;
