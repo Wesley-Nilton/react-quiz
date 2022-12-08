3ca915c7eaee1f987d5157ed767c893e6f7b4765
Wesley Nilton <wesleynlimac@gmail.com>
2022-12-08 16:25:51 -0300

    Show current question

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -5,7 +5,7 @@ function Question(){
     const [quizState, dispatch] = useContext(QuizContext);
     return(
         <div id='question'>
-            <p>Pergunta 0 de 10</p>
+            <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
             <h2>Pergunta atual</h2>
             <div id="opstions-container">
                 <p>Opções</p>
