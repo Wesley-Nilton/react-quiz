caa6f2d9b20949ff30edcf0f60eb3345dd6231ef
Wesley Nilton <wesleynlimac@gmail.com>
2022-12-19 14:03:11 -0300

    Add gameover component structure

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -5,8 +5,12 @@ import './GameOver.css';
 
 function GameOver(){
     return(
-        <div>
-            <h2>Game Over</h2>
+        <div id='gameover'>
+            <h2>Fim de Jogo</h2>
+            <p>Pontuação: x</p>
+            <p>Você acertou y de z perguntas.</p>
+            <img src={WellDone} alt="Fim do Quiz" />
+            <button>Reiniciar</button>
         </div>
     )
 }
